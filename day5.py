21d6f37ed394e5c874bf18a875120a35636f3bbb
rainvector <vectorrain@163.com>
2018-12-06 17:08:42 +0800

    day6

--- day5.py
+++ day5.py
@@ -59,6 +59,7 @@ print(shortest)
 
 
 # imporved method
+"""
 import sys
 polymers = open("day5.txt").read()
 shortest = sys.maxsize
@@ -86,6 +87,67 @@ for index in range(65,91):
         shortest = len(polymer)
         
 print(shortest)
-            
+"""
+
+"""
+总体思路，直接讲26种配对方式给找出来，然后利用str的replace函数将成对的消去，简单直接
+Ref: https://www.reddit.com/r/adventofcode/comments/a3912m/2018_day_5_solutions/eb4e5kl
+"""
+import time
+# efficient way of part 1
+line = open("day5.txt").read().splitlines()[0]
+
+start = time.time()
+oldline = None
+while oldline != line:
+    oldline = line
+    for i in range(0,26):
+        line = line.replace(chr(ord("a") + i) + chr(ord("A") + i),"")
+        line = line.replace(chr(ord("A") + i) + chr(ord("a") + i),"")
+end = time.time()
+print("Part 1 time:", end-start)
+print("Part1:")
+print(len(line))         
+
+# efficient way of part 2
+original = line
+best = len(line)
+start = time.time()
+for j in range(0,26):
+    line = original
+    line = line.replace(chr(ord("a") + j),"")
+    line = line.replace(chr(ord("A") + j),"")
+    oldline = None
+    while oldline != line:
+        oldline = line
+        for i in range(0,26):
+            line = line.replace(chr(ord("a") + i) + chr(ord("A") + i),"")
+            line = line.replace(chr(ord("A") + i) + chr(ord("a") + i),"")
+
+    best = len(line) if len(line) < best else best
+    
+end = time.time()
+print("Part 1 time:", end-start)
+print("Part2:")
+print(best)
                  
-                
+"""
+另一个思路：
+相当于两个指针一直前进，如果发现了相同的就从str里pop出去。
+Ref: https://www.reddit.com/r/adventofcode/comments/a3912m/2018_day_5_solutions/eb4jzni
+"""    
+from string import *
+def collapse(s):
+    p = ['.']
+    for u in s:
+        v = p[-1]
+        if v != u and v.lower() == u.lower():
+            p.pop()
+        else:
+            p.append(u)
+    return len(p) - 1
+
+
+s = open('day5.txt').read().strip()
+print(collapse(s))
+print(min(collapse(c for c in s if c.lower() != x) for x in ascii_lowercase))    
---
+++ day6.py
@@ -0,0 +1,167 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  6 13:09:13 2018
+
+@author: HV
+"""
+import sys
+def computeDistance(coor, point):
+    return abs(point[0] - coor[0]) + abs(point[1] - coor[1])
+
+def computeTotoalDis(coor, points):
+    distance = 0
+    for point in points:
+        distance += computeDistance(coor,point)
+    return distance
+
+
+
+file = open("day6.txt")
+
+# 读入的各个采样点
+points = list()
+for line in file.readlines():
+    [x,y] = map(int, line.split(','))
+    points.append([x,y])
+    
+[xlist, ylist]  = zip(*points)
+[minx,miny,maxx,maxy] = [min(xlist),min(ylist),max(xlist),max(ylist)]
+print(minx, miny,maxx,maxy)
+
+
+
+
+# part 1
+# grid  key: coordinates value: [pointIndex,mindistance]
+grid = dict()
+for i in range(minx,maxx+1):
+    for j in range(miny,maxy+1):
+        grid[(i,j)] = [-1,sys.maxsize];
+# 找到网格上每个坐标 离得最近的那个点的索引值和距离值， 如果同时离两个或者以上的点距离相同，则不属于任何一个点，pointIndex设置成-2
+for gridCoord in grid:
+    # 当网格上坐标离两个point最小距离相同，则跳过
+    if(grid[gridCoord][0] == -2):
+        continue
+    
+    mincoordindex = grid[gridCoord][0]
+    mindistance   = grid[gridCoord][1]
+
+    for index in range(len(points)):
+        position = points[index]
+        distance = computeDistance(gridCoord,position)
+        if distance < mindistance:
+            mindistance = distance
+            mincoordindex = index
+        elif distance == mindistance:
+            mincoordindex = -2
+    
+    grid[gridCoord]  = [mincoordindex,mindistance]
+    
+# boundaryPointset:边界上的点的 pointIndex
+boundaryPointset = set()
+for i in range(minx,maxx):
+    boundarypoint = grid[(i,miny)][0]
+    boundaryPointset.add(boundarypoint)
+for i in range(minx,maxx):
+    boundarypoint = grid[(i,maxy)][0]
+    boundaryPointset.add(boundarypoint)
+for j in range(miny,maxy):
+    boundarypoint = grid[(minx,j)][0]
+    boundaryPointset.add(boundarypoint)
+for j in range(miny,maxy):
+    boundarypoint = grid[(maxx,j)][0]
+    boundaryPointset.add(boundarypoint)    
+
+candidateCoord = list()    
+for i in range(len(points)):
+    if i not in boundaryPointset:
+        candidateCoord.append(points[i])
+        
+# 算出内部每个点的最大面积
+# aredic key : pointIndex value: area
+areadic = dict()
+for coord in grid:
+    # 如果在边界上，则面积无穷大，跳过计算
+    if grid[coord][0] in boundaryPointset:
+        continue
+    if grid[coord][0] in areadic:
+        areadic[grid[coord][0]] += 1
+    else:
+        areadic[grid[coord][0]] = 1
+
+# sortedArea key:area value: pointIndex
+sortedArea = sorted(areadic.items(),key = lambda x : x[1])
+for i in sortedArea:
+    print(i)
+"""
+[x,y] = [0,0]
+for point in points:
+    x += point[0]
+    y += point[1]
+
+origin = [int(x/len(points)),int(y/len(points))]
+region = 0
+for x in range(origin[0],maxx):
+    distance = computeTotoalDis([x, origin[1]],points)
+    if distance < 10000:
+        region += 1
+    else:
+        break
+    for y in range(origin[1]+1,maxy):
+        distance = computeTotoalDis([x,y],points)
+        if(distance < 10000):
+            region +=1
+        else:
+            break
+    
+for x in range(origin[0],maxx):
+    distance = computeTotoalDis([x, origin[1]-1],points)
+    if distance < 10000:
+        region += 1
+    else:
+        break
+    for y in reversed(range(miny,origin[1]-1)):
+        distance = computeTotoalDis([x,y],points)
+        if(distance < 10000):
+            region +=1
+        else:
+            break
+
+for x in reversed(range(minx,origin[0]-1)):
+    distance = computeTotoalDis([x, origin[1]],points)
+    if distance < 10000:
+        region += 1
+    else:
+        break
+    for y in range(origin[1]+1,maxy):
+        distance = computeTotoalDis([x,y],points)
+        if(distance < 10000):
+            region +=1
+        else:
+            break
+
+for x in reversed(range(minx,origin[0]-1)):
+    distance = computeTotoalDis([x, origin[1]],points)
+    if distance < 10000:
+        region += 1
+    else:
+        break
+    for y in reversed(range(miny,origin[1]-1)):
+        distance = computeTotoalDis([x,y],points)
+        if(distance < 10000):
+            region +=1
+        else:
+            break
+
+print(region)
+"""
+
+# part 2
+region = 0
+for x in range(minx,maxx+1):
+    for y in range(miny,maxy+1):
+        distance = computeTotoalDis([x,y],points)
+        if(distance < 10000):
+            region += 1
+         
+print (region)
