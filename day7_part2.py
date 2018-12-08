c1359dc054e9da855c81da07a4b588819fb8166a
rainvector <vectorrain@163.com>
2018-12-08 16:34:30 +0800

    day8

--- day7_part2.py
+++ day7_part2.py
@@ -28,10 +28,8 @@ def getUnconstraintChars(pairs, chars):
             
     return sorted(candidateCharList)
 
-result = str("")
-time = 0
-# 建立任务列表
-tasks = dict()
+# 总时间
+totalTime = 0
 points = set()
 for point in seq:
     if point[0] not in points:
@@ -39,73 +37,77 @@ for point in seq:
     if point[1] not in points:
         points.add(point[1])
 
+# 建立任务列表
+tasks = dict()
 for point in points:
     # Example
     #tasks[point] = ord(point) - ord('A') + 1 
     # Real
     tasks[point] = 60 + ord(point) - ord('A') + 1 
 
-
 avaliableWorkers = 5
-works = list() # 正在执行的工作
+currentWorks = list() # 正在执行的工作
 while(len(seq) > 0):
-    # 有待选的任务
+    # 有待选的任务, 这个求待选任务的函数输入参数应该不包含目前的 已经在做的工作
     candidateTasks= getUnconstraintChars(seq, chars)
     
     # 待选任务分配给可用的工人
     if  avaliableWorkers >=len(candidateTasks):
         #从待选的任务中拿出前 avaliableWorkers 个 来完成
-        works += candidateTasks[:avaliableWorkers]
+        currentWorks += candidateTasks[:avaliableWorkers]
         avaliableWorkers -= len(candidateTasks)
     else:
-        # 工人数 没有工作 多
+        # 工人数 < 任务数
         # 那就拿出 工作人数那么多个工作出来
-        works += candidateTasks[:avaliableWorkers]
+        currentWorks += candidateTasks[:avaliableWorkers]
         avaliableWorkers = 0
         
+        
+    # 从chars 和 seq 中 清除 currentWork 对应的工作
+    for work in currentWorks:
+       if work in chars:
+           chars.remove(work)
+           
+    temSeq = list()
+    for item in seq:
+        if item[0] not in  currentWorks:
+            temSeq.append(item)    
+    seq = temSeq
+    
     # 对工作的时间进行排序，最快的先完成，从tasks中剔除出去，（注意同时完成的工作）
-    minWorkTime = 100
+    minWorkTime = 1000
     minWork = list()
-    # 找到最快能够完成的时间
-    for work in works:
+    # 找到所有目前工作中 最快能够完成的时间
+    for work in currentWorks:
         workTime = tasks[work]
         if workTime < minWorkTime:
             minWorkTime = workTime
     # 找到最快能够完成的工作/任务        
-    for work in works:
+    for work in currentWorks:
         workTime = tasks[work]
         if workTime == minWorkTime:
             minWork.append(work)
 
     # 大家都疯狂干工作，工作时间是 minWorkTime
-    for task in works:
-        if tasks[task] > 0:
-            tasks[task] -= minWorkTime
+    for work in currentWorks:
+        if tasks[work] > 0:
+            tasks[work] -= minWorkTime
     
     # 从work里清楚已经完成的工作
     for i in minWork:
-        if i in works:
-            works.remove(i)
+        if i in currentWorks:
+            currentWorks.remove(i)
     
     # 总的运行时间
-    time += minWorkTime
+    totalTime += minWorkTime
     
     # 工作做完了，空出来我们的工人
     avaliableWorkers += len(minWork)
     
-   # 从chars 和 seq 中 清除 minWork 对应的工作
-    temSeq = list()
-    for item in seq:
-        if item[0] in  minWork:
-            if item[0] in chars:
-                chars.remove(item[0])
-        else:
-            temSeq.append(item)
-            
-    seq = temSeq
+
     
 # 最后一个工作的时间
 for task in tasks:
     if tasks[task] != 0:
-        time += tasks[task]
-print(time)
+        totalTime += tasks[task]
+print(totalTime)
---
+++ day8.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  8 13:05:07 2018
+
+@author: HV
+"""
+"""
+Tree Definition
+"""    
+class Tree(object):
+    def __init__(self,numChild = 0, numMetaData = 1):
+        self.numChild = numChild
+        self.numMetaData = numMetaData
+        self.children = []
+        self.metaData = []
+    
+    def insertChild(self,node):
+        self.children.append(node)
+        
+    def insertMetaData(self, metaData):
+        self.metaData += metaData
+        
+    def getNumChildren(self):
+        return self.numChild
+    
+    def getNumMetaData(self):
+        return self.numMetaData
+    
+    def getChildren(self):
+        return self.children
+    
+    def getMetaData(self):
+        return self.metaData
+ 
+"""
+Create Tree from input data
+"""
+def createTree(data): 
+    [numChild, numMetaData] = data[0:2]
+    root = Tree(numChild, numMetaData)
+    
+    for i in range(numChild):
+        node , tmpdata = createTree(data[2:])
+        root.insertChild(node)
+        data = data[:2] + tmpdata
+        
+    root.insertMetaData(data[2:2+numMetaData])
+    data = data[2+numMetaData:]
+    
+    return root,data
+    
+"""
+Traver the tree to get sum of all metadata entries
+"""
+def traverTree(tree):
+    total = 0
+    total += sum(tree.getMetaData())
+    for i in range(tree.getNumChildren()):
+        total += traverTree(tree.getChildren()[i])
+    return total
+  
+"""
+Traver the tree to get value of the root node
+"""
+def computeValueofRoot(tree):
+    valueofNode = 0
+    # if it's leaf node, compute the value from metadata and return
+    if(tree.getNumChildren() == 0):
+        valueofNode += sum(tree.getMetaData())
+        return valueofNode
+
+    metaData = tree.getMetaData()
+    for index in metaData:
+        if index <= tree.getNumChildren():
+            child = tree.getChildren()[index-1]
+            valueofNode += computeValueofRoot(child)
+    
+    return valueofNode
+
+test = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"
+data =list(map(int, open("day8.txt","r").read().split(" ")))
+#data = list(map(int,test.split(" ")))
+root, data = createTree(data)
+# part 1
+print(traverTree(root))
+# part 2
+print(computeValueofRoot(root)) 
